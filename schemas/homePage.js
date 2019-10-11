86f8e46d591bee50c3e0eef62090e1ba78176f06
gagansaini1212 <gagansaini1212@gmail.com>
2019-10-11 13:10:00 +0530

    added home page

---
+++ schemas/homePage.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'homePage',
+  title: 'Home Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'mainTitle',
+      title: 'Main Title',
+      type: 'string',
+    },
+    {
+      name: 'mainSubtitle',
+      title: 'Main Subtitle',
+      type: 'string',
+    },
+  ],
+};
