f9a37fe4a9d48f6809a1d9972cdbf4a79aa1cd05
gagansaini1212 <gagansaini1212@gmail.com>
2019-10-14 14:38:08 +0530

    added home page fields

--- schemas/homePage.js
+++ schemas/homePage.js
@@ -2,15 +2,41 @@ export default {
   name: 'homePage',
   title: 'Home Page',
   type: 'document',
+  __experimental_actions: [
+    // 'create',
+    'update',
+    // 'delete',
+    'publish',
+  ],
   fields: [
     {
-      name: 'mainTitle',
-      title: 'Main Title',
+      name: 'heroTitle',
+      title: 'Hero Title',
       type: 'string',
     },
     {
-      name: 'mainSubtitle',
-      title: 'Main Subtitle',
+      name: 'heroSubtitle',
+      title: 'Hero Subtitle',
+      type: 'string',
+    },
+    {
+      name: 'socialHeading',
+      title: 'Social Heading',
+      type: 'string',
+    },
+    {
+      name: 'socialSubheading',
+      title: 'Social Subheading',
+      type: 'string',
+    },
+    {
+      name: 'subscribeFormHeading',
+      title: 'Subscribe Form Heading',
+      type: 'string',
+    },
+    {
+      name: 'subscribeFormSubheading',
+      title: 'Subscribe Form Subheading',
       type: 'string',
     },
   ],
